81c6fb50f9fe36d9fe91fc4f0465fc1186e7462d
Milan Simonovic <mbsimonovic@gmail.com>
2018-10-03 08:29:34 +0200

    better (express) error handlers

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const path = require('path');
 const favicon = require('serve-favicon');
+const bunyan = require('bunyan');
 
 const routes = require('./routes/index');
 const groupRoute = require('./routes/groups');
 const proteinRoute = require('./routes/protein');
+
+const log = bunyan.createLogger({
+    name: 'paxdb-API-orthologs',
+    module: 'app'
+});
 
 const app = express();
 
@@ -19,33 +25,42 @@ app.use('/group', groupRoute);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
+    log.warn({
+        msg: `'${req.url}' Not Found`,
+        req
+    });
     const err = new Error('Not Found');
     err.status = 404;
     next(err);
 });
 
-// error handlers
+// error-handling middleware, take the same form
+// as regular middleware, however they require an
+// arity of 4, aka the signature (err, req, res, next).
+// when connect has an error, it will invoke ONLY error-handling
+// middleware.
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use((err, req, res) => {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
+// If we were to next() here any remaining non-error-handling
+// middleware would then be executed, or if we next(err) to
+// continue passing the error, only error-handling middleware
+// would remain being executed. Here we simply respond with an error page.
 
-// production error handler
-// no stacktraces leaked to user
-app.use((err, req, res) => {
+app.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
+    log.error({ err });
     res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+    // production error handler - no stacktraces leaked to user:
+    const params = { message: 'Looks like something went wrong!' };
+    // development error handler - will print stacktrace
+    if (app.get('env') === 'development') {
+        params.message = `Error: ${err.message}`;
+        params.error = err;
+    }
+    res.render('error', params);
+});
+
+process.on('uncaughtException', (err) => {
+    log.fatal(err, 'uncaughtException');
+    process.exit(1);
 });
 
 
